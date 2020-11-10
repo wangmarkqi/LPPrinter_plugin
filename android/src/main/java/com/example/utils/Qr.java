3abc2cadd36fbcc876ff2bc847d450a50039b8dd
wangmarkqi <wangalexqi@126.com>
2020-11-10 17:27:18 +0800

    upgrade jar

--- android/src/main/java/com/example/utils/Qr.java
+++ android/src/main/java/com/example/utils/Qr.java
@@ -9,27 +9,31 @@ import com.dothantech.lpapi.LPAPI.Factory;
 
 public class Qr {
     private LPAPI api;
-    public void onCreate(){
+
+    public void onCreate() {
         this.api = LPAPI.Factory.createInstance();
         // 连接配对的第一个打印机对象
         api.openPrinter(null);
     }
-    public void onDestroy(){
+
+    public void onDestroy() {
         // 断开蓝牙连接，释放 LPAPI 对象
         api.quit();
     }
-    public void printQr(String title,String qr,String txt) {
+
+    public void printQr(String title, String qr, String txt) {
         // 开始绘图任务，传入参数(页面宽度, 页面高度 默认40*30)
-       api.startJob(40, 30, 0);
-        api.drawTextRegular(title, 27, 1, 12, 4, 4,1);
-        api.drawTextRegular(txt, 27, 5, 12, 25, 3.2,4);
+        api.startJob(40, 30, 0);
+        api.drawTextRegular(title, 27, 1, 12, 4, 4, 1);
+        api.drawTextRegular(txt, 27, 5, 12, 25, 3.2, 4);
         api.draw2DQRCode(qr, 1, 1, 25);
         api.commitJob();
     }
-    public void printQr75_50(String title,String qr,String txt) {
+
+    public void printQr75_50(String title, String qr, String txt) {
         api.startJob(75, 50, 0);
-        api.drawTextRegular(title, 2, 1, 70, 6, 6,1);
-        api.drawTextRegular(txt, 43, 8, 32, 42, 4,4);
+        api.drawTextRegular(title, 2, 1, 70, 6, 6, 1);
+        api.drawTextRegular(txt, 43, 8, 32, 42, 4, 4);
         api.draw2DQRCode(qr, 2, 8, 40);
         api.commitJob();
     }
